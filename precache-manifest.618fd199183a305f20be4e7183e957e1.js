b21f8f51d1375a702b33ac52c1fba348da367f0f
Ludo <ludovic.descateaux@student.ecp.fr>
2019-10-08 14:17:48 +0200

    Updates

---
+++ precache-manifest.618fd199183a305f20be4e7183e957e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5a8f2a712f95df43913cb118f27f89",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "61bfd0fac9cf35f1a0de",
+    "url": "/react-tutorial/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3694d84116a6b3760b8a",
+    "url": "/react-tutorial/static/js/2.4cad404d.chunk.js"
+  },
+  {
+    "revision": "61bfd0fac9cf35f1a0de",
+    "url": "/react-tutorial/static/js/main.797eb93f.chunk.js"
+  },
+  {
+    "revision": "266279a793cf3a171621",
+    "url": "/react-tutorial/static/js/runtime-main.5cabd5d6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-tutorial/static/media/logo.25bf045c.svg"
+  }
+]);
