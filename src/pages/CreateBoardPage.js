4994164a27fc79ab20c23e1814a7ee00cf26e7ad
brieuc <brieuc.loussouarn@orange.fr>
2019-10-09 11:29:00 +0200

    fix eslint issue

--- src/pages/CreateBoardPage.js
+++ src/pages/CreateBoardPage.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function CreateBoardPage() {
     return (
         <div className="container">
-            <h1>Create Board Page</h1>
+            <h1>Create Board Page </h1>
             <p>
                 <Link to="/lounge">Back to Lounge page</Link> aaaaahh....
             </p>
