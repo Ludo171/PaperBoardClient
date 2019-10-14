5e1cb64f885f459c9e41751f44932daaa47f73b0
Ludo <ludovic.descateaux@student.ecp.fr>
2019-10-14 13:58:12 +0200

    Welcome-page first version

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,40 +1,66 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import * as backgroundImage from '../assets/background-image.jpg';
+import * as backgroundImage from "../assets/background-image1.jpg";
 import "./WelcomePage.scss";
 
-export default function WelcomePage() {
+export default class WelcomePage extends React.Component {
+    resizeImageToFill(e) {
+        let targetWidth;
+        let targetHeight;
+        const container = document.getElementById("welcome-page");
+        const containerProp = container.clientHeight / container.clientWidth;
+        const img = document.getElementById("background-image");
+        const imgProp = img.height / img.width;
 
-    // const img = new Image();
-    // img.onload = function() {
-    //     const root = document.getElementById("root");
-    //     const pageProp = root.clientHeight / root.clientWidth;
-    //     const imgProp = this.imageW
-    // };
+        if (containerProp > imgProp) {
+            targetHeight = container.clientHeight;
+            targetWidth = targetHeight / imgProp;
+        } else {
+            targetWidth = container.clientWidth;
+            targetHeight = targetWidth * imgProp;
+        }
 
-    return (
-        <div id="welcome-page" className="container">
-            <img id="background-image" src={backgroundImage} alt="background"></img>
-            <div className="box">
-                <p className="title is-1">Welcome on PaperBoard !</p>
-                <p className="subtitle is-4">Don't think to much, draw it !</p>
-            </div>
-            <div className="box">
-                <div className="field">
-                    <p className="control has-icons-left">
-                        <input className="input" type="password" placeholder="Password"></input>
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"></i>
-                        </span>
-                    </p>
+        img.style.width = `${Math.floor(targetWidth)}px`;
+        img.style.height = `${Math.floor(targetHeight)}px`;
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resizeImageToFill);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resizeImageToFill);
+    }
+
+    render() {
+        return (
+            <div id="welcome-page">
+                <img
+                    id="background-image"
+                    src={backgroundImage}
+                    onLoad={this.resizeImageToFill}
+                    alt="background"></img>
+                <div className="box">
+                    <p className="title is-1">Welcome on PaperBoard !</p>
+                    <p className="subtitle is-4">Don't think to much, draw it !</p>
                 </div>
-                <div className="field">
-                    <p className="control">
-                        <button className="button is-success">Login</button>
-                    </p>
+                <div className="box">
+                    <div className="field">
+                        <p className="control has-icons-left">
+                            <input className="input" type="password" placeholder="Password"></input>
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-lock"></i>
+                            </span>
+                        </p>
+                    </div>
+                    <div className="field">
+                        <p className="control">
+                            <button className="button is-success">Login</button>
+                        </p>
+                    </div>
+                    <Link to="/lounge">Go to Lounge page</Link> youpi.
                 </div>
-                <Link to="/lounge">Go to Lounge page</Link> youpi.
             </div>
-        </div>
-    );
+        );
+    }
 }
