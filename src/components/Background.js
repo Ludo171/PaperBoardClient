13acfee3d987b8983e580b6c358aab01bbcd43fe
brieuc <brieuc.loussouarn@orange.fr>
2019-10-15 16:16:56 +0200

    clean up welcome page and create background component

---
+++ src/components/Background.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import * as backgroundImage from "../assets/background-image2.jpg";
+import PropTypes from "prop-types";
+import "./Background.scss";
+
+class Background extends Component {
+    resizeImageToFill(e) {
+        let targetWidth;
+        let targetHeight;
+        const container = document.getElementById("page");
+        const containerProp = container.clientHeight / container.clientWidth;
+        const img = document.getElementById("background-image");
+        const imgProp = img.height / img.width;
+
+        if (containerProp > imgProp) {
+            targetHeight = container.clientHeight;
+            targetWidth = targetHeight / imgProp;
+        } else {
+            targetWidth = container.clientWidth;
+            targetHeight = targetWidth * imgProp;
+        }
+
+        img.style.width = `${Math.floor(targetWidth)}px`;
+        img.style.height = `${Math.floor(targetHeight)}px`;
+    }
+    componentDidMount() {
+        window.addEventListener("resize", this.resizeImageToFill);
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resizeImageToFill);
+    }
+    render() {
+        return (
+            <div id="page">
+                <img
+                    id="background-image"
+                    src={backgroundImage}
+                    onLoad={this.resizeImageToFill}
+                    alt="background"></img>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+Background.propTypes = {
+    children: PropTypes.object,
+};
+export default Background;
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,39 +1,14 @@
 import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
-import * as backgroundImage from "../assets/background-image2.jpg";
 import "./WelcomePage.scss";
 import PropTypes from "prop-types";
 import {postUser} from "../services/users";
+import Background from "../components/Background";
 
 class WelcomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {pseudo: ""};
-    }
-    resizeImageToFill(e) {
-        let targetWidth;
-        let targetHeight;
-        const container = document.getElementById("welcome-page");
-        const containerProp = container.clientHeight / container.clientWidth;
-        const img = document.getElementById("background-image");
-        const imgProp = img.height / img.width;
-
-        if (containerProp > imgProp) {
-            targetHeight = container.clientHeight;
-            targetWidth = targetHeight / imgProp;
-        } else {
-            targetWidth = container.clientWidth;
-            targetHeight = targetWidth * imgProp;
-        }
-
-        img.style.width = `${Math.floor(targetWidth)}px`;
-        img.style.height = `${Math.floor(targetHeight)}px`;
-    }
-    componentDidMount() {
-        window.addEventListener("resize", this.resizeImageToFill);
-    }
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.resizeImageToFill);
     }
     onLogin = (event) => {
         event.preventDefault();
@@ -58,12 +33,7 @@ class WelcomePage extends Component {
     };
     render() {
         return (
-            <div id="welcome-page">
-                <img
-                    id="background-image"
-                    src={backgroundImage}
-                    onLoad={this.resizeImageToFill}
-                    alt="background"></img>
+            <Background>
                 <div className="card">
                     <div className="card-content">
                         <p className="title is-1">Welcome on PaperBoard !</p>
@@ -82,9 +52,6 @@ class WelcomePage extends Component {
                                                     value={this.state.value}
                                                     onChange={this.handleChange}></input>
                                             </form>
-                                            {/* <span className="icon is-small is-left">
-                                                <i className="fa material-icons">face</i>
-                                            </span> */}
                                         </p>
                                     </div>
                                     <div className="field">
@@ -101,7 +68,7 @@ class WelcomePage extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </Background>
         );
     }
 }
