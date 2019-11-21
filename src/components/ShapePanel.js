b03e5826da52982393f641b18577554b874e125b
brieuc <brieuc.loussouarn@orange.fr>
2019-11-21 15:38:05 +0100

    move sidelist in an other component

--- src/components/ShapePanel.js
+++ src/components/ShapePanel.js
@@ -1,44 +1,79 @@
-import React, {Component} from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import {ColorLens} from "@material-ui/icons";
-import {Icon} from "@material-ui/core";
+import {
+    TextFields as TextFieldIcon,
+    Photo,
+    Maximize,
+    CropLandscape,
+    Edit,
+    PanoramaFishEye,
+} from "@material-ui/icons";
+import {Icon, ListSubheader} from "@material-ui/core";
 
-class ShapePanel extends Component {
-    changeColor = () => {
-        this.props.editCircle();
-        alert("color changed");
-    };
+const shapePanel = (createTextField, createCircle) => (
+    <div style={{maxWidth: 250, backgroundColor: "white"}}>
+        <List
+            component="nav"
+            aria-label="main mailbox folders"
+            subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            fontSize: "2em",
+                        }}>
+                        Tools
+                    </div>
+                </ListSubheader>
+            }>
+            <ListItem button key={"Text"} onClick={createTextField}>
+                <ListItemIcon>
+                    <TextFieldIcon />
+                </ListItemIcon>
+                <ListItemText primary={"Text"} />
+            </ListItem>
+        </List>
+        <Divider />
+        <List>
+            {[
+                {title: "Line", component: <Maximize />},
+                {title: "Rectangle", component: <CropLandscape />},
+                {title: "Circle", component: <PanoramaFishEye />, method: createCircle},
+                {
+                    title: "Draw",
+                    component: <Edit />,
+                },
+                {
+                    title: "Triangle",
+                    component: (
+                        <Icon>
+                            <img src={require("../assets/triangle.png")} alt="" />
+                        </Icon>
+                    ),
+                },
+            ].map((item) => (
+                <ListItem button key={item.title} onClick={item.method}>
+                    <ListItemIcon>{item.component}</ListItemIcon>
+                    <ListItemText primary={item.title} />
+                </ListItem>
+            ))}
+        </List>
+        <Divider />
+        <List>
+            <ListItem button key={"Picture"}>
+                <ListItemIcon>
+                    <Photo />
+                </ListItemIcon>
+                <ListItemText primary={"Picture"} />
+            </ListItem>
+        </List>
+    </div>
+);
 
-    render() {
-        return (
-            <div style={{maxWidth: 360, backgroundColor: "white"}}>
-                <List component="nav" aria-label="main mailbox folders">
-                    {[
-                        {title: "Color", component: <ColorLens />, method: this.changeColor},
-                        {
-                            title: "Width",
-                            component: (
-                                <Icon>
-                                    <img src={require("../assets/width.png")} alt="" />
-                                </Icon>
-                            ),
-                        },
-                    ].map((item) => (
-                        <ListItem button key={item.title} onClick={item.method}>
-                            <ListItemIcon>{item.component}</ListItemIcon>
-                            <ListItemText primary={item.title} />
-                        </ListItem>
-                    ))}
-                </List>
-            </div>
-        );
-    }
-}
-ShapePanel.propTypes = {
-    editCircle: PropTypes.any,
-};
-export default ShapePanel;
+export default shapePanel;
--- src/pages/PaperBoardPage.js
+++ src/pages/PaperBoardPage.js
@@ -2,19 +2,7 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {withRouter} from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import {
-    TextFields as TextFieldIcon,
-    Photo,
-    Maximize,
-    CropLandscape,
-    Edit,
-    PanoramaFishEye,
-} from "@material-ui/icons";
+
 import Background from "../components/Background";
 import {ExitToApp, SaveAlt, CloudUpload, Message as MessageIcon} from "@material-ui/icons";
 import Chat from "../components/Chat";
@@ -24,47 +12,12 @@ import constants from "../config/constants";
 import Message from "../components/Message";
 import ListOfUsers from "../components/ListOfUsers";
 import CanvasManager from "../components/CanvasManager";
-import ShapePanel from "../components/ShapePanel";
+import EditShapePanel from "../components/EditShapePanel";
 import Toast from "light-toast";
 import * as backgroundImage from "../assets/cappuccino2.jpg";
+import shapePanel from "../components/ShapePanel";
 
 const color = require("string-to-color");
-
-const sideList = (createTextField, createCircle) => (
-    <div style={{maxWidth: 250, backgroundColor: "white"}}>
-        <List component="nav" aria-label="main mailbox folders">
-            <ListItem button key={"Text"} onClick={createTextField}>
-                <ListItemIcon>
-                    <TextFieldIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Text"} />
-            </ListItem>
-        </List>
-        <Divider />
-        <List>
-            {[
-                {title: "Line", component: <Maximize />},
-                {title: "Rectangle", component: <CropLandscape />},
-                {title: "Circle", component: <PanoramaFishEye />, method: createCircle},
-                {title: "Edit", component: <Edit />},
-            ].map((item) => (
-                <ListItem button key={item.title} onClick={item.method}>
-                    <ListItemIcon>{item.component}</ListItemIcon>
-                    <ListItemText primary={item.title} />
-                </ListItem>
-            ))}
-        </List>
-        <Divider />
-        <List>
-            <ListItem button key={"Picture"}>
-                <ListItemIcon>
-                    <Photo />
-                </ListItemIcon>
-                <ListItemText primary={"Picture"} />
-            </ListItem>
-        </List>
-    </div>
-);
 
 class PaperBoardPage extends Component {
     constructor(props) {
@@ -327,7 +280,7 @@ class PaperBoardPage extends Component {
                         flex: 1,
                     }}>
                     {/* ACTION BUTTONS NAV BAR */}
-                    {sideList(this.createTextField, this.createCircle)}
+                    {shapePanel(this.createTextField, this.createCircle)}
 
                     {/* CANVAS MANAGER */}
                     <CanvasManager
@@ -338,7 +291,7 @@ class PaperBoardPage extends Component {
                     />
 
                     {/* SHAPE OPTIONS PANEL */}
-                    {isShapePanelToggeled && <ShapePanel editCircle={this.editCircle} />}
+                    {isShapePanelToggeled && <EditShapePanel editCircle={this.editCircle} />}
                 </div>
                 {/* CHAT */}
                 <div
